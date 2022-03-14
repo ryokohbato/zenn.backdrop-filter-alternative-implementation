7d709fb78dd52dbe00e3855904bf76d3721bf910
ryokohbato <ryokohbato@gmail.com>
2022-03-14 19:12:05 +0900

    background プロパティを継承し続ける代替実装

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,28 +1,53 @@
 <template>
-  <div id="app">
+  <div id="app" :style="backgroundSize">
     <LoginForm/>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
 import LoginForm from "./components/LoginForm.vue";
 
-@Component({
+export default {
+  data () {
+    return {
+      backgroundHeight: window.innerHeight * 0.75 < window.innerWidth ? window.innerWidth * 4 / 3 : window.innerHeight,
+      backgroundWidth: window.innerHeight * 0.75 < window.innerWidth ? window.innerWidth : window.innerHeight * 0.75,
+    }
+  },
+  computed: {
+    backgroundSize: function () {
+      return {
+        backgroundSize: `${this.backgroundWidth}px ${this.backgroundHeight}px`,
+        // backgroundSize: `cover`,
+      };
+    }
+  },
+  methods: {
+    resizeHandler: function () {
+      if (window.innerHeight * 0.75 < window.innerWidth) {
+        this.backgroundHeight = window.innerWidth * 4 / 3;
+        this.backgroundWidth = window.innerWidth;
+      } else {
+        this.backgroundHeight = window.innerHeight;
+        this.backgroundWidth = window.innerHeight * 0.75;
+      }
+    }
+  },
+  mounted () {
+    window.addEventListener('resize', this.resizeHandler);
+  },
+  beforeDestroy () {
+    window.removeEventListener('resize', this.resizeHandler);
+  },
   components: {
     LoginForm,
   },
-})
-export default class App extends Vue {}
+}
 </script>
 
 <style lang="scss">
 html {
   background-color: #222;
-  background-image: url("./assets/background.jpg");
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
   height: 100%;
   width: 100%;
 }
@@ -32,10 +57,20 @@ body {
 }
 
 #app {
+  background-image: url("./assets/background.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  height: calc(100vh - 60px);
   text-align: center;
-  margin-top: 60px;
+  padding-top: 60px;
+  width: 100vw;
+
+  .container {
+    margin-left: 50%;
+    transform: translateX(-50%);
+  }
 }
 </style>
--- app/src/components/LoginForm.vue
+++ app/src/components/LoginForm.vue
@@ -1,15 +1,18 @@
 <template>
-  <div class="form">
-    <img class="icon" src="../assets/ryokohbato.png" alt="" />
-    <p class="name">ryokohbato</p>
-    <div class="password">
-      <input
-        class="password__input"
-        type="password"
-        name="password"
-        id="password"
-        placeholder="PASSWORD"
-      />
+  <div class="container">
+    <div class="glassed-background"></div>
+    <div class="form">
+      <img class="icon" src="../assets/ryokohbato.png" alt="" />
+      <p class="name">ryokohbato</p>
+      <div class="password">
+        <input
+          class="password__input"
+          type="password"
+          name="password"
+          id="password"
+          placeholder="PASSWORD"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -21,64 +24,88 @@ export default class LoginForm extends Vue {}
 </script>
 
 <style scoped lang="scss">
-.form {
+.container {
   $form-width: 400px;
-  background-color: #2228;
-  backdrop-filter: saturate(1.5) blur(12px);
+  background: inherit;
+  background-position: 50% calc(50% + ((100vh - 500px) / 2 - 60px));
   border-radius: 8px;
-  display: inline-block;
   height: 500px;
+  overflow: hidden;
+  position: relative;
   width: $form-width;
 
-  .icon {
-    border-radius: 50%;
-    height: calc($form-width * 0.5);
-    margin-top: 60px;
-    width: calc($form-width * 0.5);
+
+  .glassed-background {
+    background: inherit;
+    filter: blur(12px) brightness(1.2);
+    position: absolute;
+    left: -12px;
+    right: -12px;
+    top: -12px;
+    bottom: -12px;
   }
 
-  .name {
-    color: white;
-    font-size: 28px;
-    font-weight: bold;
-  }
-
-  .password {
+  .form {
+    background-color: #2228;
+    border-radius: 8px;
     display: inline-block;
-    width: 60%;
+    height: 500px;
+    left: 0;
+    position: absolute;
+    top: 0;
+    width: $form-width;
+    z-index: 10;
 
-    &__input {
-      background-color: #7773;
-      border-radius: 4px;
-      border: none;
-      border-bottom: 2px solid white;
-      color: white;
-      font-size: 28px;
-      padding: 0 12px;
-      width: 80%;
-
-      &::placeholder {
-        font-size: 16px;
-        color: #fff9;
-        transform: translateY(-2px);
-      }
-
-      &:focus {
-        background-color: #aaa3;
-        border-bottom: 2px solid #42b983;
-        outline: 0;
-        transition: all .3s;
-      }
+    .icon {
+      border-radius: 50%;
+      height: calc($form-width * 0.5);
+      margin-top: 60px;
+      width: calc($form-width * 0.5);
     }
 
-    &__submit {
-      border-radius: 4px;
-      border: 1px solid white;
-      font-size: 18px;
+    .name {
+      color: white;
+      font-size: 28px;
       font-weight: bold;
-      margin-top: 140px;
-      padding: 5px 60px;
-      width: 80%;
+    }
+
+    .password {
+      display: inline-block;
+      width: 60%;
+
+      &__input {
+        background-color: #7773;
+        border-radius: 4px;
+        border: none;
+        border-bottom: 2px solid white;
+        color: white;
+        font-size: 28px;
+        padding: 0 12px;
+        width: 80%;
+
+        &::placeholder {
+          font-size: 16px;
+          color: #fff9;
+          transform: translateY(-2px);
+        }
+
+        &:focus {
+          background-color: #aaa3;
+          border-bottom: 2px solid #42b983;
+          outline: 0;
+          transition: all .3s;
+        }
+      }
+
+      &__submit {
+        border-radius: 4px;
+        border: 1px solid white;
+        font-size: 18px;
+        font-weight: bold;
+        margin-top: 140px;
+        padding: 5px 60px;
+        width: 80%;
+      }
     }
   }
 }
